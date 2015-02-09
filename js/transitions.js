2dd305cc67cb64802a9eee78086a1631de2a2295
jayschaul <jay@jayschaul.com>
2015-02-09 12:44:14 -0500

    no java on mobile

--- js/transitions.js
+++ js/transitions.js
@@ -1,19 +1,26 @@
-$(function() {
-  setTimeout(function() {
-    $('#subtitle, h2').fadeIn(1000, function(){
-      setTimeout(fadeProjects, 1300);
-    });
-  },125);
-});
+function isMobile() {
+return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
 
-function fadeProjects() {
-  var projects = $('#projects');
+if (!isMobile()) {
 
-  $(projects).each(function(i){
-    var $self = $(this);
+  $(function() {
     setTimeout(function() {
-      $self.show();
-      $self.addClass('animated fadeInDown');
-    }, i * 200);
+      $('#subtitle, h2').fadeIn(1000, function(){
+        setTimeout(fadeProjects, 1300);
+      });
+    },125);
   });
+
+  function fadeProjects() {
+    var projects = $('#projects');
+
+    $(projects).each(function(i){
+      var $self = $(this);
+      setTimeout(function() {
+        $self.show();
+        $self.addClass('animated fadeInDown');
+      }, i * 200);
+    });
+  }
 }
