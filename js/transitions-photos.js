f719ec0891bf6181e1a37fe009f3906175ce3cdc
jayschaul <jay@jayschaul.com>
2016-06-05 22:35:41 -0400

    load in of text and photos

---
+++ js/transitions-photos.js
@@ -0,0 +1,33 @@
+window.onload = function() {
+  document.getElementById('page-title').className = 'header fadingin';
+  document.getElementById('image-container').className = 'images fadingin';
+};
+
+// function isMobile() {
+// return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+// }
+
+// if (!isMobile()) {
+
+  // $(function() {
+  //   setTimeout(function() {
+  //     $('#subtitle, h2').fadeIn(1000, function(){
+  //       setTimeout(fadeProjects, 1300);
+  //     });
+  //   },125);
+  // });
+
+
+
+  // function fadeProjects() {
+  //   var projects = $('#projects');
+
+  //   $(projects).each(function(i){
+  //     var $self = $(this);
+  //     setTimeout(function() {
+  //       $self.show();
+  //       $self.addClass('animated fadeInDown');
+  //     }, i * 200);
+  //   });
+  // }
+// }
